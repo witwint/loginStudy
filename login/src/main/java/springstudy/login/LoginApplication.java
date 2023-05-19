62c954c1e8d2926179629d244a7a19bc6f7956d9
witwint <witwint1234@gmail.com>
2023-05-19 20:21:46 +0900

    [feat] 초기세팅

---
+++ login/src/main/java/springstudy/login/LoginApplication.java
@@ -0,0 +1,14 @@
+package springstudy.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplication.class, args);
+	}
+
+}
